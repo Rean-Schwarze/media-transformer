84f405154798b403e80081a875f3e03fdd846b68
Rean <1626877909@qq.com>
2024-04-08 21:01:50 +0800

    feat: 视频转换 - 上传 & 播放 & 设置

---
+++ src/stores/video.ts
@@ -0,0 +1,88 @@
+import {defineStore} from "pinia";
+import {ref, type Ref} from "vue";
+import {ElMessage} from "element-plus";
+
+export const useVideoStore=defineStore('video',()=>{
+    const videoTable:Ref<Object[]>=ref([])
+
+    const addVideoTable=(name:string, type:string)=>{
+        const videoData:{name:string, type:string, title:string}= {
+            type: type==='x-matroska'?'mkv':type || type==='quicktime'?'mov':type,
+            name:name,
+            title:name.split('.')[0]
+        }
+        const isExists=videoTable.value.some(item=>item.name===name)
+        if(!isExists){
+            videoTable.value.push(videoData)
+        }
+        else{
+            ElMessage.warning('该文件已添加于列表中！')
+        }
+    }
+
+    const clearVideoTable=()=>{
+        videoTable.value.splice(0,videoTable.value.length)
+        ElMessage.success('视频列表已清空！')
+    }
+
+    const deleteRow=(index:number, row:{name:string})=>{
+        let title=row.name
+        videoTable.value.splice(index,1)
+        ElMessage.warning('成功删除：'+title)
+    }
+
+    const setOptions=(index:number,option:{exportFormat:string,mode:string,bit:number,resolution:string,frame:string,transcodeMode:string,preset:string,crf:number})=>{
+        const updateItem = (item: any) => {
+            item.exportFormat = option.exportFormat;
+            item.mode=option.mode;
+            item.bit=option.bit
+            item.resolution=option.resolution
+            item.frame=option.frame
+            item.transcodeMode=option.transcodeMode
+            item.preset=option.preset
+            item.crf=option.crf
+            return item;
+        };
+
+        if (index === -1) {
+            videoTable.value = videoTable.value.map(updateItem);
+        } else {
+            videoTable.value[index] = updateItem(videoTable.value[index]);
+        }
+        ElMessage.success('设置成功！')
+    }
+
+    const addExport=(index:number,exportPath:string, exportName:string)=>{
+        videoTable.value[index].exportPath=exportPath
+        videoTable.value[index].exportName=exportName
+    }
+
+    const checkOption = (index: number) => {
+        if (index === -1) {
+            return videoTable.value.every(row => {
+                return typeof row.exportFormat === 'undefined'
+                    || (row.exportFormat!=='audio' && typeof row.mode==='undefined')
+                    || (row.mode==='transcode' && typeof row.transcodeMode==='undefined')
+                    || (row.transcodeMode==='preset' && typeof row.preset==='undefined');
+            });
+        } else {
+            const { exportFormat ,mode,transcodeMode,preset} = videoTable.value[index] as { exportFormat: string,mode:string,transcodeMode:string,preset:string};
+            return typeof exportFormat === 'undefined' || (exportFormat!=='audio' && typeof mode==='undefined')
+                || (mode==='transcodeMode' && typeof transcodeMode==='undefined')
+                || (transcodeMode==='preset' && typeof preset==='undefined');
+        }
+    }
+
+
+    return{
+        videoTable,
+        addVideoTable,
+        clearVideoTable,
+        deleteRow,
+        setOptions,
+        addExport,
+        checkOption,
+    }
+},{
+    persist:true,
+})
--- src/views/Video/index.vue
+++ src/views/Video/index.vue
@@ -1,11 +1,438 @@
 <script setup lang="ts">
+import {Delete, Download, Position, Setting, UploadFilled, VideoPlay} from '@element-plus/icons-vue'
+import {
+  type UploadFile,
+  type UploadFiles,
+  type UploadInstance,
+  type UploadProps,
+  type UploadUserFile,
+} from "element-plus";
+import {onUnmounted, ref} from 'vue'
+import {useVideoStore} from "@/stores/video";
+import {useFFMpegStore} from '@/stores/ffmpegStore'
+import { fetchFile, toBlobURL } from '@ffmpeg/util'
+
+const uploadRef = ref<UploadInstance>()
+const videoList=ref<UploadUserFile[]>([])
+const videoStore=useVideoStore()
+const videoTable=videoStore.videoTable
+
+const allowedFileTypes = ['video/mp4', 'video/quicktime', 'video/x-matroska'];
+
+const ffmpegStore=useFFMpegStore()
+const ffmpeg=ffmpegStore.ffmpeg
+
+onUnmounted(()=>videoStore.clearVideoTable())
+
+// 上传相关
+const videoUpload=(uploadFile: UploadFile, uploadFiles: UploadFiles)=>{
+  let type=uploadFile.raw?.type.split('/')[1]
+  if (!allowedFileTypes.includes(uploadFile.raw?.type!)) {
+    if(uploadFile.raw?.name.split('.')[1]!=='flv'){
+      uploadRef.value!.handleRemove(uploadFile);
+      return
+    }
+    else{
+      type='flv'
+    }
+  }
+  videoStore.addVideoTable(<string>uploadFile.raw?.name,<string>type)
+}
+
+const handleExceed: UploadProps['onExceed'] = () => {
+  ElMessage.warning(
+      "目前只能上传10个文件，请重试"
+  )
+}
+
+const handleRemove: UploadProps['onRemove']=(file, fileList)=>{
+  ElMessage.error(
+      "当前上传文件 "+file.name+' 格式不受支持！'
+  )
+}
+
+const handleDelete=(index:number,row: { title:string,name:string })=>{
+  if(index===-1){
+    videoList.value=[]
+    videoStore.clearVideoTable()
+  }
+  else{
+    videoList.value=videoList.value.filter(obj=>obj.name!==row.name)
+    videoStore.deleteRow(index,row)
+  }
+}
+
+// 播放相关
+const dialogVideoVisible=ref(false)
+const videoRef=ref()
+const handlePlay=async (index:number,row:{name:string})=>{
+  dialogVideoVisible.value=true
+  const raw:File=videoList.value.find(obj=>obj.name===row.name)?.raw
+  const blob=await new Blob([raw],{type:'video/mp4'})
+  videoRef.value.src=URL.createObjectURL(blob)
+}
+
+// 设置相关
+const dialogConfigVisible=ref(false)
+const selectedIndex=ref(-1)
+const selectedOption=ref({exportFormat:'',mode:'',resolution:'',frame:'', bit:1000,transcodeMode:'',preset:'',crf:18})
+
+const formatOptions=[
+  {
+    value:'audio',
+    label:'抽取音频（复制）',
+  },
+  {
+    value:'mp4',
+    label:'mp4',
+  },
+  {
+    value:'mov',
+    label:'mov',
+  },
+]
+
+const modeOption=[
+  {
+    value:'transcode',
+    label:'转换编码'
+  },
+  {
+    value:'transmux',
+    label:'转换容器'
+  }
+]
+
+const sizeOption=[
+  {
+    value:'',
+    label:'与源相同'
+  },
+  {
+    value:'-1:480',
+    label:'480p'
+  },
+  {
+    value:'-1:720',
+    label:'720p'
+  },
+  {
+    value:'-1:1080',
+    label:'1080p'
+  },
+  {
+    value:'-1:1440',
+    label:'2K'
+  },
+  {
+    value:'-1:2160',
+    label:'4K'
+  }
+]
+
+const frameOption=[
+  {
+    value:'',
+    label:'与源相同'
+  },
+  {
+    value:'24',
+    label:'24'
+  },
+  {
+    value:'25',
+    label:'25'
+  },
+  {
+    value:'30',
+    label:'30'
+  },
+  {
+    value:'60',
+    label:'60'
+  }
+]
+
+const transcodeOption=[
+  {
+    value:'-preset',
+    label:'预设'
+  },
+  {
+    value:'-crf',
+    label:'crf（恒定速率因子模式）'
+  },
+  {
+    value:'-b',
+    label:'固定比特率'
+  }
+]
+
+const presetOption = [
+  {
+    value: 'ultrafast',
+    label: 'ultrafast'
+  },
+  {
+    value: 'superfast',
+    label: 'superfast'
+  },
+  {
+    value: 'veryfast',
+    label: 'veryfast'
+  },
+  {
+    value: 'faster',
+    label: 'faster'
+  },
+  {
+    value: 'fast',
+    label: 'fast'
+  },
+  {
+    value: 'medium',
+    label: 'medium'
+  },
+  {
+    value: 'slow',
+    label: 'slow'
+  },
+  {
+    value: 'slower',
+    label: 'slower'
+  },
+  {
+    value: 'veryslow',
+    label: 'veryslow'
+  }
+];
+
+
+const handleConfig=(index:number,row:{exportFormat:string,mode:string,bit:number,resolution:string,frame:string,transcodeMode:string,preset:string,crf:number})=>{
+  dialogConfigVisible.value=true
+  selectedIndex.value=index
+  if(index===-1){
+    selectedOption.value={exportFormat:'',mode:'',resolution:'',frame:'', bit:1000,transcodeMode:'',preset:'',crf:18}
+  }
+  else{
+    selectedOption.value.exportFormat=row.exportFormat
+    selectedOption.value.mode=row.mode
+    selectedOption.value.bit=row.bit
+    selectedOption.value.resolution=row.resolution
+    selectedOption.value.transcodeMode=row.transcodeMode
+    selectedOption.value.preset=row.preset
+    selectedOption.value.crf=row.crf
+  }
+}
+const handleConfirm=()=>{
+  videoStore.setOptions(selectedIndex.value,selectedOption.value)
+  dialogConfigVisible.value=false
+}
+
+// 处理相关
 
 </script>
 
 <template>
-video页
+  <div class="audio-container">
+<!--    上传区域-->
+    <el-upload
+        class="upload" ref="uploadRef"
+        drag multiple
+        accept=".mp4, .mov, .flv, .mkv"
+        :on-change="videoUpload"
+        :on-remove="handleRemove"
+        :limit="10"
+        v-model:file-list="videoList"
+        :auto-upload="false"
+        :on-exceed="handleExceed"
+        :show-file-list="false"
+    >
+      <el-icon class="el-icon--upload"><upload-filled /></el-icon>
+      <div class="el-upload__text">
+        将文件拖到此处，或 <em>点击选择</em>
+      </div>
+    </el-upload>
+
+<!--    批量区域-->
+    <div class="option">
+      <el-row justify="space-evenly">
+        <el-col :span="3">
+          <el-button size="large" type="danger" :icon="Delete" @click="handleDelete(-1,{})">
+            清空
+          </el-button>
+        </el-col>
+        <el-col :span="11">
+          <audio ref="audioRef" controls autoplay style="height:40px; width:90%;">
+          </audio>
+        </el-col>
+        <el-col :span="3">
+          <el-button size="large" type="info" :icon="Setting" @click="handleConfig(-1,{})">
+            批量设置
+          </el-button>
+        </el-col>
+        <el-col :span="3">
+          <el-button size="large" type="primary" :icon="Position" @click="handleProcess(-1,{})">
+            批量处理
+          </el-button>
+        </el-col>
+        <el-col :span="3">
+          <el-button size="large" type="primary" :icon="Download" @click="handleDownload(-1,{})">
+            批量下载
+          </el-button>
+        </el-col>
+      </el-row>
+    </div>
+
+<!--    展示表格-->
+    <el-table :data="videoTable" :table-layout="'auto'" class="table" :header-cell-style="{'text-align':'center'}"
+              stripe empty-text="暂无数据">
+      <el-table-column prop="title" label="文件名" align="center"/>
+      <el-table-column prop="type" label="格式" align="center"/>
+      <el-table-column prop="exportFormat" label="输出格式" align="center"/>
+      <el-table-column label="操作" align="center">
+        <template #default="scope">
+          <div class="table-button">
+            <el-button size="default" @click="handlePlay(scope.$index, scope.row)" :icon="VideoPlay"></el-button>
+            <el-button size="default" type="info" @click="handleConfig(scope.$index, scope.row)" :icon="Setting"></el-button>
+            <el-button
+                size="default"
+                type="primary" :icon="Position"
+                @click="handleProcess(scope.$index, scope.row)"
+            ></el-button>
+            <el-button
+                size="default"
+                type="primary" :icon="Download"
+                @click="handleDownload(scope.$index, scope.row)"
+            ></el-button>
+            <el-button
+                size="default"
+                type="danger" :icon="Delete"
+                @click="handleDelete(scope.$index, scope.row)"
+            ></el-button>
+          </div>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+
+<!--  设置Dialog-->
+  <el-dialog v-model="dialogConfigVisible" title="转换设置" width="330px">
+    <div class="dialog-row">
+      <el-text style="display:inline-flex; width:80px; padding-left: 15px;">
+        输出格式
+      </el-text>
+      <el-select v-model="selectedOption.exportFormat" placeholder="输出格式" size="large" style="width:200px; padding-left: 25px;">
+        <el-option v-for="item in formatOptions" :key="item.value" :label="item.label" :value="item.value" />
+      </el-select>
+    </div>
+    <div :class="selectedOption.exportFormat!=='audio'?'dialog-row':'dialog-row-hidden'">
+      <el-text style="display:inline-flex; width:80px; padding-left: 15px;">
+        转换模式
+      </el-text>
+      <el-select v-model="selectedOption.mode" placeholder="转换模式" size="large" style="width:200px; padding-left: 25px;">
+        <el-option v-for="item in modeOption" :key="item.value" :label="item.label" :value="item.value" />
+      </el-select>
+    </div>
+    <div :class="selectedOption.mode==='transcode'?'dialog-row':'dialog-row-hidden'">
+      <el-text style="display:inline-flex; width:80px; padding-left: 15px;">
+        分辨率
+      </el-text>
+      <el-select v-model="selectedOption.resolution" placeholder="分辨率" size="large" style="width:200px; padding-left: 25px;">
+        <el-option v-for="item in sizeOption" :key="item.value" :label="item.label" :value="item.value" />
+      </el-select>
+    </div>
+    <div :class="selectedOption.mode==='transcode'?'dialog-row':'dialog-row-hidden'">
+      <el-text style="display:inline-flex; width:80px; padding-left: 15px;">
+        帧率
+      </el-text>
+      <el-select v-model="selectedOption.frame" placeholder="帧率" size="large" style="width:200px; padding-left: 25px;">
+        <el-option v-for="item in frameOption" :key="item.value" :label="item.label" :value="item.value" />
+      </el-select>
+    </div>
+    <div :class="selectedOption.mode==='transcode'?'dialog-row':'dialog-row-hidden'">
+      <el-text style="display:inline-flex; width:80px; padding-left: 15px;">
+        编码模式
+      </el-text>
+      <el-select v-model="selectedOption.transcodeMode" placeholder="编码模式" size="large" style="width:200px; padding-left: 25px;">
+        <el-option v-for="item in transcodeOption" :key="item.value" :label="item.label" :value="item.value" />
+      </el-select>
+    </div>
+    <div :class="selectedOption.transcodeMode==='-preset'?'dialog-row':'dialog-row-hidden'">
+      <el-text style="display:inline-flex; width:80px; padding-left: 15px;">
+        预设
+      </el-text>
+      <el-select v-model="selectedOption.preset" placeholder="预设" size="large" style="width:200px; padding-left: 25px;">
+        <el-option v-for="item in presetOption" :key="item.value" :label="item.label" :value="item.value" />
+      </el-select>
+    </div>
+    <div :class="selectedOption.transcodeMode==='-crf'?'dialog-row-slider':'dialog-row-hidden'">
+      <el-text style="display:inline-flex; width:80px; padding-left: 15px;">
+        crf
+      </el-text>
+      <el-slider v-model="selectedOption.crf" :step="1" :min="18" :max="28" style="padding-left:25px; width:200px;"/>
+    </div>
+    <div :class="selectedOption.transcodeMode==='-b'?'dialog-row-slider':'dialog-row-hidden'">
+      <el-text style="display:inline-flex; width:80px; padding-left: 15px;">
+        比特率(kbps)
+      </el-text>
+      <el-input-number v-model="selectedOption.bit" :step="500" min="1000" max="12000" controls-position="right"
+                       size="large" style="padding-left:25px; width:200px;" />
+    </div>
+
+    <template #footer>
+      <el-button @click="dialogConfigVisible=false">取消</el-button>
+      <el-button type="primary" @click="handleConfirm">确认</el-button>
+    </template>
+  </el-dialog>
+
+<!--  播放视频Dialog-->
+  <el-dialog v-model="dialogVideoVisible">
+    <video ref="videoRef" controls autoplay style="height:100%; width:100%; padding-top:10px;">
+    </video>
+  </el-dialog>
 </template>
 
 <style scoped>
+.audio-container{
+  margin: auto;
+  align-items: center;
+  overflow: auto;
 
+  .upload{
+    margin: auto;
+    width:40%;
+    -webkit-app-region:no-drag;
+  }
+
+  .option{
+    padding-top: 10px;
+    padding-left: 5px;
+    -webkit-app-region:no-drag;
+  }
+
+  .table{
+    padding-top: 5px;
+    -webkit-app-region:no-drag;
+  }
+
+  .table-button{
+    padding-top: 10px;
+    margin:auto;
+  }
+}
+
+.dialog-row{
+  padding-top:5px;
+}
+
+.dialog-row-slider{
+  display:inline-flex;
+  padding-top: 10px;
+  max-width: 330px;
+}
+
+.dialog-row-hidden{
+  display:none;
+}
 </style>

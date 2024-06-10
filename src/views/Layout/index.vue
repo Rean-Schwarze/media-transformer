b7a24a3266930084654193ff4538a1076ce8e1ea
Rean <1626877909@qq.com>
2024-06-10 22:43:53 +0800

    style(header): 冻结header

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -10,12 +10,23 @@ onMounted(async ()=> await ffmpegStore.initFFMpeg()  )
 </script>
 
 <template>
-  <LayoutHeader />
+  <LayoutHeader class="fixed-header" />
   <!--  二级路由出口  -->
-  <RouterView />
+  <RouterView class="content" />
   <LayoutFooter />
 </template>
 
 <style scoped>
+.fixed-header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 98%;
+  z-index: 1000; /* 确保它在其他内容之上 */
+  background-color: var(--el-bg-color);
+}
 
+.content{
+  padding-top: 60px;
+}
 </style>
